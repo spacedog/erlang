fc4113a0c9104450540d00cbebae4c5dc7178cdd
anton baranov <abaranov@linux.com>
2015-01-27 15:30:48 -0500

    math_functions: added split1/1 and split2/1 functions

--- math_functions.erl
+++ math_functions.erl
@@ -1,5 +1,5 @@
 -module(math_functions).
--export([even/1, odd/1, filter/2]).
+-export([even/1, odd/1, filter/2, split1/1,split2/1]).
 
 
 even(X) when (X rem 2) =:= 0 -> true;
@@ -11,4 +11,21 @@ odd(_) -> false.
 filter(_,[])    -> [];
 filter(F,L) ->
   [X || X <- L, F(X) =:= true].
+
+
+split1(L) ->
+  Odd  = filter(fun(X) -> odd(X) end, L),
+  Even = filter(fun(X) -> even(X) end, L),
+  {Even, Odd}.
+
+split2(L) ->
+  split_acc(L,[],[]).
+
+split_acc([H|T], Even, Odd) ->
+  case H rem 2 of
+    0 -> split_acc(T, [H|Even],Odd);
+    1 -> split_acc(T, Even, [H|Odd])
+  end;
+split_acc([], Even, Odd) ->
+  {lists:reverse(Even), lists:reverse(Odd)}.
 
