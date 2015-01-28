337b2183ac7ca42741c72beeac503c898dd60ede
anton baranov <abaranov@linux.com>
2015-01-28 14:36:05 -0500

    try_test: created test module

---
+++ try_test.erl
@@ -0,0 +1,25 @@
+-module(try_test).
+-export([demo1/0,demo2/0]).
+
+
+generate_exception(1) -> a;
+generate_exception(2) -> throw(a);
+generate_exception(3) -> exit(a);
+generate_exception(4) -> {'EXIT', a};
+generate_exception(5) -> error(a).
+
+
+demo1() ->
+  [catcher(I) || I <- lists:seq(1,5) ].
+
+catcher(N) ->
+  try generate_exception(N) of
+    Val -> {N, normal, Val}
+  catch
+    throw:X -> {N, caught, thrown, X};
+    exit:X -> {N, caught, exited, X};
+    error:X -> {N, caught, error, X}
+  end.
+
+demo2() ->
+  [{I, (catch generate_exception(I))} || I <- lists:seq(1,5)].
