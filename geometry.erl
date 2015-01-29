7d55e4d2ebf623b70abfa251c87d9ce03b79241e
anton baranov <abaranov@linux.com>
2015-01-29 15:17:08 -0500

    geometry: added catch-all clause for area function.

--- geometry.erl
+++ geometry.erl
@@ -4,5 +4,6 @@
 area({rectangle, Width, Height}) -> Width * Height;
 area({circle, Radius})           -> 3.14159 * Radius * Radius;
 area({square, Side})             -> Side * Side;
-area({triangle, A, B})           -> A*B / 2.
+area({triangle, A, B})           -> A*B / 2;
+area(_)                          -> {error, invalid_object}.
 
