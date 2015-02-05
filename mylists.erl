1436262c6d057e12550d11f089efdc1966168844
anton baranov <abaranov@linux.com>
2015-02-05 14:04:45 -0500

    mylists: create/1 function

--- mylists.erl
+++ mylists.erl
@@ -1,8 +1,22 @@
 -module(mylists).
--export([sum/1, map/2]).
+-export([
+         sum/1,
+         map/2,
+         create/1
+        ]).
 
 sum([H|T]) -> H + sum(T);
 sum([])    -> 0.
 
 map(F,[H|T]) -> [F(H)|map(F,T)];
 map(_,[])    -> [].
+
+
+create(N) when N >= 0 ->
+  create(1,N);
+create(_) -> [].
+
+create(X,N) when X =< N ->
+  [X|create(X+1,N)];
+create(N,N) -> N;
+create(_,_) -> [].
