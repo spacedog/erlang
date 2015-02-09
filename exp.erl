698ee84d92af7262e69a792ecbe2e5087cdadf31
anton baranov <abaranov@linux.com>
2015-02-09 10:20:09 -0500

    exp: renamed functions. compile/1 function

--- exp.erl
+++ exp.erl
@@ -1,7 +1,8 @@
 - module(exp).
 - export([
-          exp/1,
-          print/1
+          evaluator/1,
+          pretty_print/1,
+          compile/1
          ]).
 
 p_lb() -> io:format("(").
@@ -11,17 +12,29 @@ p_pl() -> io:format(" + ").
 p_mu() -> io:format(" * ").
 p_dv() -> io:format(" / ").
 p_un() -> io:format("~~").
-exp({num,       N         }) -> N;
-exp({minus,     Exp1, Exp2}) -> exp(Exp1) - exp(Exp2);
-exp({plus,      Exp1, Exp2}) -> exp(Exp1) + exp(Exp2);
-exp({multiple,  Exp1, Exp2}) -> exp(Exp1) * exp(Exp2);
-exp({devide,    Exp1, Exp2}) -> exp(Exp1) / exp(Exp2);
-exp({unary,     Exp1      }) -> - exp(Exp1).
 
-print({num,       N         }) -> io:format("~p",[N]);
-print({minus,     Exp1, Exp2}) -> p_lb(), print(Exp1), p_mi(), print(Exp2), p_rb();
-print({plus,      Exp1, Exp2}) -> p_lb(), print(Exp1), p_pl(), print(Exp2), p_rb();
-print({multiple,  Exp1, Exp2}) -> p_lb(), print(Exp1), p_mu(), print(Exp2), p_rb();
-print({devide,    Exp1, Exp2}) -> p_lb(), print(Exp1), p_dv(), print(Exp2), p_rb();
-print({unary,     Exp1      }) -> p_un(), print(Exp1).
+evaluator({num,       N         }) -> N;
+evaluator({minus,     Exp1, Exp2}) -> evaluator(Exp1) - evaluator(Exp2);
+evaluator({plus,      Exp1, Exp2}) -> evaluator(Exp1) + evaluator(Exp2);
+evaluator({multiple,  Exp1, Exp2}) -> evaluator(Exp1) * evaluator(Exp2);
+evaluator({devide,    Exp1, Exp2}) -> evaluator(Exp1) / evaluator(Exp2);
+evaluator({unary,     Exp1      }) -> - evaluator(Exp1).
 
+pretty_print({num,       N         }) -> io:format("~p",[N]);
+pretty_print({minus,     Exp1, Exp2}) -> p_lb(), pretty_print(Exp1), p_mi(), pretty_print(Exp2), p_rb();
+pretty_print({plus,      Exp1, Exp2}) -> p_lb(), pretty_print(Exp1), p_pl(), pretty_print(Exp2), p_rb();
+pretty_print({multiple,  Exp1, Exp2}) -> p_lb(), pretty_print(Exp1), p_mu(), pretty_print(Exp2), p_rb();
+pretty_print({devide,    Exp1, Exp2}) -> p_lb(), pretty_print(Exp1), p_dv(), pretty_print(Exp2), p_rb();
+pretty_print({unary,     Exp1      }) -> p_un(), pretty_print(Exp1).
+
+
+compile(Exp) -> compile(Exp,[]).
+
+compile({num, Num},         Acc) -> [Num | Acc];
+compile({Oper, Exp1, Exp2}, Acc) -> lists:concat([compile(Exp1),
+                                                  compile(Exp2),
+                                                  [Oper | Acc]
+                                                 ]);
+compile({unary, Exp},       Acc) -> lists:concat([compile(Exp),
+                                                  [unary | Acc]
+                                                 ]).
